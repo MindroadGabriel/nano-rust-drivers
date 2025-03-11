18c569dea59f52ebee5b8f604661a6de047c28ce
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-03-11 14:55:42 +0100

    Add debouncing and fixed slice vec

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,26 @@ mod print;
 mod ssd1306_registers;
 mod ssd1306_error;
 mod ssd1306_font;
+mod debouncing;
 
 use core::cell::RefCell;
+use core::mem::MaybeUninit;
 use panic_halt as _;
 use arduino_hal::Peripherals;
 use arduino_hal::prelude::*;
+use crate::debouncing::Debouncer;
+use crate::debouncing::DebounceResult::Pressed;
 use crate::ssd1306::BUFFER_SIZE;
 use crate::ssd1306_registers::WHITE;
+
+enum GameState {
+    Menu,
+    Displaying,
+    Inputting,
+    Next,
+    Failure,
+    Score,
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -27,6 +40,7 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
     let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
     print::put_console(serial);
+    let mut led = pins.d13.into_output();
 
     let result = (|| -> Result<(), error::UDisplayError<arduino_hal::i2c::Error>> {
         let mut i2c = arduino_hal::I2c::new(
@@ -35,26 +49,11 @@ fn main() -> ! {
             pins.a5.into_pull_up_input(),
             50000,
         );
-        let mut led = pins.d13.into_output();
         let mut button1 = pins.d5.into_pull_up_input();
         let mut button2 = pins.d4.into_pull_up_input();
-        let mut button1_last_pressed = button1.is_low();
-        let mut button2_last_pressed = button2.is_low();
         let i2c_ref_cell = RefCell::new(i2c);
-        let mut accelerometer = bmi160::Driver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, None, |ms| arduino_hal::delay_ms(ms))?;
         let mut buffer = [0x00; ssd1306::BUFFER_SIZE];
         let display_result = ssd1306::DisplayDriver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, &mut buffer);
-        // print::print_type_name(&display_result);
-        //
-        // core::result::Result<
-        // hackathon_pong_controller::ssd1306::DisplayDriver<embedded_hal_bus::i2c::refcell::RefCellDevice<avr_hal_generic::i2c::I2c<atmega_hal::Atmega,
-        // avr_device::devices::atmega328p::TWI,
-        // avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Input,
-        // atmega_hal::port::PC4>,
-        // avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Input,
-        // atmega_hal::port::PC5>,
-        // avr_hal_generic::clock::MHz16>>>,
-        // hackathon_pong_controller::ssd1306_error::Error<avr_hal_generic::i2c::Error>>
         let mut display = match display_result {
             Ok(display) => {
                 display
@@ -63,84 +62,49 @@ fn main() -> ! {
                 return Err(error.into());
             }
         };
-
-        // println!("BMI160 initialized");
+        let mut debouncer_storage = [0x00; 2];
+        let mut debouncer = Debouncer::new(&mut debouncer_storage[..]);
         println!("Started");
 
-        // loop {
-        //     // let button1_pressed = button1.is_low();
-        //     // let button2_pressed = button2.is_low();
-        //     // if button1_pressed && !button1_last_pressed {
-        //     //     println!("Button1 pressed");
-        //     // }
-        //     // if button2_pressed && !button2_last_pressed {
-        //     //     println!("Button2 pressed");
-        //     // }
-        //     // button1_last_pressed = button1_pressed;
-        //     // button2_last_pressed = button2_pressed;
-        //     //
-        //     // led.toggle();
-        //     // accelerometer.update()?;
-        //     // if let Some(output_data) = accelerometer.get_output_data() {
-        //     // }
-        //     println!("On");
-        //     display.fill_screen(ssd1306_registers::WHITE);
-        //     display.display()?;
-        //     led.set_high();
-        //     arduino_hal::delay_ms(2000);
-        //
-        //     println!("Off");
-        //     display.fill_screen(ssd1306_registers::BLACK);
-        //     display.display()?;
-        //     led.set_low();
-        //     arduino_hal::delay_ms(2000);
-        // }
-        loop {
-            display.fill_screen(ssd1306_registers::BLACK);
-            for i in 0..32 {
-                display.draw_pixel(i, i, WHITE)?;
-            }
-            for i in 0..32 {
-                display.draw_pixel(31 + i, 31, WHITE)?;
-            }
-            for i in 0..32 {
-                display.draw_pixel(63 + i, 31 - i, WHITE)?;
-            }
-            for i in 0..32 {
-                display.draw_pixel(95 + i, 0, WHITE)?;
-            }
-            display.display()?;
-            arduino_hal::delay_ms(1000);
-            display.draw_line(4, 4, 57, 25, WHITE)?;
-            display.display()?;
-            arduino_hal::delay_ms(1000);
-            display.draw_line(57, 4, 4, 25, WHITE)?;
-            display.display()?;
-            arduino_hal::delay_ms(1000);
-            display.draw_line(97, 15, 44, 30, WHITE)?;
-            display.display()?;
-            arduino_hal::delay_ms(1000);
-            display.draw_line(44, 15, 97, 30, WHITE)?;
-            display.display()?;
-            arduino_hal::delay_ms(1000);
-            display.draw_fill_rect(4, 4, 57 - 4, 25 - 4, WHITE)?;
-            display.display()?;
-            // display.dim(true)?;
-            arduino_hal::delay_ms(1000);
-            // display.dim(false)?;
-            arduino_hal::delay_ms(1000);
+        const MAX_SEQUENCE: usize = 128;
+        let mut sequence_storage = [MaybeUninit::new(false); MAX_SEQUENCE];
+        let mut sequence = fixed_slice_vec::FixedSliceVec::new(&mut sequence_storage[..]);
+        sequence.clear();
+        let game_state = GameState::Menu;
+        let mut next_guess_index = 0;
+        let mut highest_cleared = 0;
+        let mut first = true;
+        sequence.push(true);
 
-            display.clear_display();
-            display.set_cursor(0, 0);
-            // display.draw_string("You're a mean one,\nMister Grinch!");
-            display.draw_string(" Abc");
-            display.display()?;
-            arduino_hal::delay_ms(2000);
+        loop{
+            // if button1.is_low() {
+            //     println!("B1 low");
+            // }
+            let button1_state = debouncer.update(0, button1.is_low());
+            let button2_state = debouncer.update(1, button2.is_low());
+            if button1_state == Pressed {
+                println!("B1");
+            }
+            if button2_state == Pressed {
+                println!("B2");
+            }
+
+            println!("Loop");
+            led.set_high();
+            arduino_hal::delay_ms(1000);
+            led.set_low();
+            arduino_hal::delay_ms(1000);
         }
     })();
     if let Err(error) = result {
         #[cfg(feature = "string-errors")]
         println!("Error: {}", error);
+        loop {
+            led.set_high();
+            arduino_hal::delay_ms(100);
+            led.set_low();
+            arduino_hal::delay_ms(100);
+        }
     }
     loop {}
 }
